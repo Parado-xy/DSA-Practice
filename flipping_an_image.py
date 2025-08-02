545df5d00aa3373e9f27a7d7e6319d1bbb94bf0b
Parado-xy <jallaamaju@gmail.com>
2025-08-02 16:28:47 -0500

    LeetCode 832

---
+++ flipping_an_image.py
@@ -0,0 +1,52 @@
+# Given an n x n binary matrix image, flip the image horizontally, then invert it, and return the resulting image.
+
+# To flip an image horizontally means that each row of the image is reversed.
+
+# For example, flipping [1,1,0] horizontally results in [0,1,1].
+# To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0.
+
+# For example, inverting [0,1,1] results in [1,0,0].
+ 
+
+# Example 1:
+
+# Input: image = [[1,1,0],[1,0,1],[0,0,0]]
+# Output: [[1,0,0],[0,1,0],[1,1,1]]
+# Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
+# Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+# Example 2:
+
+# Input: image = [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+# Output: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+# Explanation: First reverse each row: [[0,0,1,1],[1,0,0,1],[1,1,1,0],[0,1,0,1]].
+# Then invert the image: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+ 
+
+# Constraints:
+
+# n == image.length
+# n == image[i].length
+# 1 <= n <= 20
+# images[i][j] is either 0 or 1.
+
+from typing import List
+
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+
+        # FLipping an Image Horizontally Means that each row in the image is reversed. 
+        # Inverting an image means that each 0 is replaced by ones and ones replaced by zeroes. 
+
+        for row in image:
+            # Reverse each row of the image (a.k.a flip horizontal) 
+            row[:] = row[::-1]
+
+        for row in image:
+            # Invert each row. 
+            row[:] = [0 if x == 1 else 1 for x in row]
+
+       
+
+        return image
+
+        
